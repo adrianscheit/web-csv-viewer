6337545406ee0ad0555ed967272b2352a9552de7
Adrian Scheit <adrianscheit@gmail.com>
2022-10-23 21:13:38 +0200

    First real use and many improvements.

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ document.querySelector('input[type=file]')!.addEventListener('input', (event) =>
   if (files.length === 1) {
     setStatus(`Reading a text file of ${files[0].size}B...`);
     const reader = new FileReader();
-    reader.onload = () => {
+    reader.onload = async () => {
       const fields: string[][] = (reader.result as string)
         .split(/\r\n|\n\r|\n|\r/)
         .map((line) => line.split(','))
         .filter((line) => line.length);
       const columns = fields.shift()!.map((title, index) => new Column(title, index));
       const data: number[][] = fields.map((line) => line.map((field) => +field));
-      setStatus(`There are ${fields.length} lines and ${columns.length} columns in the loaded file. Analysing file...`);
+      await setStatus(`There are ${fields.length} lines and ${columns.length} columns in the loaded file. Analysing file...`);
       if (fields.length < 2 || columns.length < 2) {
         setStatus('the file is too small');
         return;
@@ -22,8 +22,8 @@ document.querySelector('input[type=file]')!.addEventListener('input', (event) =>
         columns.forEach((column) => column.add(line[column.index]));
       }
       const domain: Column = columns.shift()!;
-      setStatus(`The domain column has: ${JSON.stringify(domain)}`);
-      setStatus(`The data columns: ${JSON.stringify(columns)}`);
+      await setStatus(`The domain column has: ${JSON.stringify(domain)}`);
+      await setStatus(`The data columns: ${JSON.stringify(columns)}`);
 
       // print consts:
       for (const column of columns.filter((column) => column.quantity && column.isConstant())) {
@@ -32,12 +32,12 @@ document.querySelector('input[type=file]')!.addEventListener('input', (event) =>
 
       // draw diagram:
       const diagramColumns = columns.filter((column) => !column.isConstant());
-      diagramColumns.forEach((column, index) => {
-        setStatus(`Drawing points ${index / diagramColumns.length}%. Drawing ${column.title}...`);
+      diagramColumns.forEach(async (column, index) => {
+        await setStatus(`Drawing points ${100 * index / diagramColumns.length}%. Drawing ${column.title}...`);
         addDiagram(domain, column, data);
       });
 
-      setStatus(``);
+      await setStatus(``);
     };
     reader.readAsText(files[0]);
   }
