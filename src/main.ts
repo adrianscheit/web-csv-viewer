5aa75f49c26a06caf9745dbd3814d29813478a09
Adrian Scheit <adrianscheit@gmail.com>
2022-10-25 21:46:13 +0200

    Add summary diagram

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Column } from "./Column";
-import { addConstant, addDiagram, setStatus } from "./dom";
+import { addConstant, addDiagram, addSummaryDiagram, setStatus } from "./dom";
 
-document.querySelector('input[type=file]')!.addEventListener('input', (event) => {
+document.querySelector('input[type=file]')!.addEventListener('input', async (event) => {
   const files: FileList = (event.target as any).files;
   if (files.length === 1) {
     setStatus(`Reading a text file of ${files[0].size}B...`);
     const reader = new FileReader();
-    reader.onload = () => {
+    reader.onload = async () => {
       const fields: string[][] = (reader.result as string)
         .split(/\r\n|\n\r|\n|\r/)
         .map((line) => line.split(','))
@@ -14,7 +14,7 @@ document.querySelector('input[type=file]')!.addEventListener('input', (event) =>
       const columns = fields.shift()!.map((title, index) => new Column(title, index));
       console.log(columns);
       const data: number[][] = fields.map((line) => line.map((field) => +field));
-      setStatus(`There are ${fields.length} lines and ${columns.length} columns in the loaded file. Analysing file...`);
+      await setStatus(`There are ${fields.length} lines and ${columns.length} columns in the loaded file. Analysing file...`);
       if (fields.length < 2 || columns.length < 2) {
         setStatus('the file is too small');
         return;
@@ -31,12 +31,14 @@ document.querySelector('input[type=file]')!.addEventListener('input', (event) =>
 
       // draw diagram:
       const diagramColumns = columns.filter((column) => !column.isConstant());
-      diagramColumns.forEach((column, index) => {
-        setStatus(`Drawing points ${Math.round(100 * index / diagramColumns.length)}%. Drawing ${column.title}...`);
-        addDiagram(domain, column, data);
-      });
+      for (let i = 0; i < diagramColumns.length; ++i) {
+        await setStatus(`Drawing points ${Math.round(10 + 60 * i / diagramColumns.length)}%. Drawing ${diagramColumns[i].title}...`);
+        addDiagram(domain, diagramColumns[i], data);
+      }
+      await setStatus(`Drawind summary diagram...`);
+      addSummaryDiagram(domain, diagramColumns.slice(1), data);
 
-      setStatus(`Done!`);
+      await setStatus(`Done!`);
     };
     reader.readAsText(files[0]);
   }
