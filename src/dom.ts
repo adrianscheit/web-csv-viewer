37e8dbe59de8a2cb4d1612d136175b26f2eb4585
Adrian Scheit <adrianscheit@gmail.com>
2022-10-25 18:51:08 +0200

    Use canvas to draw diagrams

--- src/dom.ts
+++ src/dom.ts
@@ -15,31 +15,27 @@ export const addConstant = (column: Column): void => {
         .appendChild(document.createTextNode(`${column.title} = ${column.avg()}`));
 };
 
-const svgNs = 'http://www.w3.org/2000/svg';
-
-const addPoint = (pDomain: number, pValue: number): SVGElement => {
-    const point = document.createElementNS(svgNs, 'rect');
-    point.setAttribute('y', `${199 - pValue * 199}`);
-    point.setAttribute('x', `${pDomain * 1999}`);
-    point.setAttribute('width', `1`);
-    point.setAttribute('height', `1`);
-    return point;
+const addPoint = (pDomain: number, pValue: number, canvasRenderingContext2D: CanvasRenderingContext2D): void => {
+    canvasRenderingContext2D.fillRect(pDomain * 2048, 256 - pValue * 256, 1, 1);
 };
 
 export const addDiagram = (domain: Column, column: Column, data: number[][]): void => {
-    const diagram = diagrams.appendChild(document.createElementNS(svgNs, 'svg'));
-    diagram.classList.add('diagram');
-    diagram.style.fill = column.color;
+    const canvas = diagrams.appendChild(document.createElement('canvas'));
+    canvas.className = 'diagram';
+    canvas.width = 2049;
+    canvas.height = 257;
+    const context = canvas.getContext('2d')!;
+    context.fillStyle = column.color;
 
     for (const line of data) {
         if (isNaN(line[domain.index]) || isNaN(line[column.index])) {
             continue;
         }
-        diagram.appendChild(addPoint(domain.getProprotion(line), column.getProprotion(line)));
+        addPoint(domain.getProprotion(line), column.getProprotion(line), context);
     }
 
     diagrams
         .appendChild(document.createElement('strong'))
         .appendChild(document.createTextNode(`${column.title}(${domain.title}) min=${column.min}, max=${column.max}, avg=${column.avg()}, color=${column.color}`))
-    diagrams.appendChild(diagram);
+    diagrams.appendChild(canvas);
 };
