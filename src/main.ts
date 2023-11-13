520ffb7131e1d0bfc096903327e6b19ad66227de
Adrian Scheit <adrianscheit@gmail.com>
2023-11-13 21:30:50 +0100

    Add fetch feature

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,64 @@
 import { Column } from "./Column";
 import { addConstant, addDiagram, addSummaryDiagram, setStatus } from "./dom";
 
+const parseFile = async (fileContent: string): Promise<void> => {
+  await setStatus(`Parsing file...`);
+
+  const fields: string[][] = fileContent
+    .split(/\r\n|\n\r|\n|\r/)
+    .map((line) => line.split(','))
+    .filter((line) => line.length);
+  const columns: Column[] = fields.shift()!.map((title, index) => new Column(title, index));
+  console.log(columns);
+  const data: number[][] = fields.map((line) => line.map((field) => +field));
+
+  await setStatus(`There are ${fields.length} lines and ${columns.length} columns in the loaded file. Analysing file data...`);
+
+  if (fields.length < 2 || columns.length < 2) {
+    setStatus('the file is too small');
+    return;
+  }
+  for (const line of data) {
+    columns.forEach((column) => column.add(line[column.index]));
+  }
+  const domain: Column = columns[0];
+
+  // print consts:
+  for (const column of columns.filter((column) => column.quantity && column.isConstant())) {
+    addConstant(column);
+  }
+
+  // draw diagram:
+  const diagramColumns = columns.filter((column) => !column.isConstant());
+  for (let i = 0; i < diagramColumns.length; ++i) {
+    await setStatus(`Drawing points ${Math.round(10 + 60 * i / diagramColumns.length)}%. Drawing ${diagramColumns[i].title}...`);
+    addDiagram(domain, diagramColumns[i], data);
+  }
+  await setStatus(`Drawind summary diagram...`);
+  addSummaryDiagram(domain, diagramColumns.slice(1), data);
+
+  await setStatus(`Done!`);
+};
+
+(async () => {
+  const params: [string, string][] = window.location.search
+    .substring(1)
+    .split('&')
+    .map((keyValue) => keyValue.split('=').map((it) => window.decodeURIComponent(it)) as [string, string]);
+
+  if (params[0][0] === 'fetch') {
+    const response = await window.fetch(params[0][1]);
+    await parseFile(await response.text());
+  }
+})();
+
 document.querySelector('input[type=file]')!.addEventListener('input', async (event) => {
   const files: FileList = (event.target as any).files;
   if (files.length === 1) {
     setStatus(`Reading a text file of ${files[0].size}B...`);
     const reader = new FileReader();
-    reader.onload = async () => {
-      await setStatus(`Parsing file...`);
-
-      const fields: string[][] = (reader.result as string)
-        .split(/\r\n|\n\r|\n|\r/)
-        .map((line) => line.split(','))
-        .filter((line) => line.length);
-      const columns: Column[] = fields.shift()!.map((title, index) => new Column(title, index));
-      console.log(columns);
-      const data: number[][] = fields.map((line) => line.map((field) => +field));
-
-      await setStatus(`There are ${fields.length} lines and ${columns.length} columns in the loaded file. Analysing file data...`);
-
-      if (fields.length < 2 || columns.length < 2) {
-        setStatus('the file is too small');
-        return;
-      }
-      for (const line of data) {
-        columns.forEach((column) => column.add(line[column.index]));
-      }
-      const domain: Column = columns[0];
-
-      // print consts:
-      for (const column of columns.filter((column) => column.quantity && column.isConstant())) {
-        addConstant(column);
-      }
-
-      // draw diagram:
-      const diagramColumns = columns.filter((column) => !column.isConstant());
-      for (let i = 0; i < diagramColumns.length; ++i) {
-        await setStatus(`Drawing points ${Math.round(10 + 60 * i / diagramColumns.length)}%. Drawing ${diagramColumns[i].title}...`);
-        addDiagram(domain, diagramColumns[i], data);
-      }
-      await setStatus(`Drawind summary diagram...`);
-      addSummaryDiagram(domain, diagramColumns.slice(1), data);
-
-      await setStatus(`Done!`);
+    reader.onload = () => {
+      parseFile(reader.result as string);
     };
     reader.readAsText(files[0]);
   }
